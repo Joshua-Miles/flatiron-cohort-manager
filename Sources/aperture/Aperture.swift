af3678d606a9577c03327c1a788a0b1fcc632dbd
Flatiron Student <flatironstudent@Flatirons-MacBook-Air.local>
2020-05-07 16:19:49 -0500

    -

--- Sources/aperture/Aperture.swift
+++ Sources/aperture/Aperture.swift
@@ -31,7 +31,7 @@ public final class Aperture: NSObject {
 	) throws {
 		self.destination = destination
 		session = AVCaptureSession()
-		session.sessionPreset = AVCaptureSession.Preset.high
+		session.sessionPreset = AVCaptureSession.Preset.hd1280x720
 
 		self.output = output
 
