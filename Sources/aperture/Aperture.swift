3f67f127666cbea49266c717b1a15c5ffc7e5faa
Flatiron Student <flatironstudent@Flatirons-MacBook-Air.local>
2020-05-07 17:26:57 -0500

    -

--- Sources/aperture/Aperture.swift
+++ Sources/aperture/Aperture.swift
@@ -90,7 +90,7 @@ public final class Aperture: NSObject {
 	*/
 	public convenience init(
 		destination: URL,
-		framesPerSecond: Int = 1,
+		framesPerSecond: Int = 40,
 		cropRect: CGRect? = nil,
 		showCursor: Bool = true,
 		highlightClicks: Bool = false,
